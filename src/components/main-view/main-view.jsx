64914d4765bae53dd3ee288660394bbe9a14e2c7
tcederberg <trcederberg@gmail.com>
2023-12-05 07:17:41 -0500

    basic navbar added

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,6 +3,7 @@ import { MovieCard } from "./../movie-card/movie-card";
 import { MovieView } from "./../movie-view/movie-view";
 import { LoginView } from "./../login-view/login-view";
 import { SignupView } from "./../signup-view/signup-view";
+import { NavigationBar } from "./../navigation-bar/navigation-bar";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
@@ -46,6 +47,12 @@ export const MainView = () => {
       
     return (
         <BrowserRouter>
+        <NavigationBar
+        user={user}
+        onLoggedOut={() => {
+          setUser(null);
+        }}
+        />
         <Row className="justify-content-md-center">
             <Routes>
                 <Route
